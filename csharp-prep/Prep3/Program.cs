f40b60ee0889fdde5dbac2b673394ac6e1164ced
Dexter <djusery@gmail.com>
2023-09-21 13:33:48 -0600

    assingment completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int Number = 5;
+        int Guess = 1;
+        while (Guess != Number )
+        {
+            Console.WriteLine("What is the magic number?");
+            Guess = int.Parse(Console.ReadLine()); 
+
+            if (Number > Guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Number < Guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } 
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Globalization;
+using System.Net;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name:");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number:");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int squaredNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squaredNumber);
     }
 }
